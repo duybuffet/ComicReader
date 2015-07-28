7aa6a3df8375ab9c0f8b9124ba4ad8de843c2a0b
phuong91193 <phuongd11cn2@gmail.com>
2015-07-28 09:33:31 +0700

    query db1

--- Sources/comic/comicreader/scripts/QueryDatabase.py
+++ Sources/comic/comicreader/scripts/QueryDatabase.py
@@ -1,5 +1,4 @@
-__author__ = 'sang'
-# -*- coding: utf-8 -*-
+__author__ = 'phuong'
 import os
 import sys
 
@@ -16,29 +15,43 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 from comicreader.models import *
+from comicreader.constants import *
+from django.db.models import Q
 
-
-def getEbook():
+def getEbooks():
+    """
+    Lay thong tin tu CSDL de lam input cho viec crawl chapter
+    :return: list object ebook
+    """
     listEbook = []
     sql= 'SELECT id, url FROM ebook'
     for eb in Ebook.objects.raw(sql):
         listEbook.append(eb)
     return listEbook
 
-def getChapter():
+def getChapters():
+    """
+    Lay thong tin tu CSDL de lam input cho viec crawl image
+    :return: list cac object chapter
+    """
     listChap = []
     sql= 'SELECT id, url FROM chapter'
     for chap in Chapter.objects.raw(sql):
         listChap.append(chap)
     return listChap
 
-def getDownload():
-    listImage = []
-    sql= 'SELECT a.id, a.name, b.id, b.name, c.id, c.name, c.url FROM ebook a, chapter b, image c WHERE a.id = b.ebook_id AND b.ebook_id = chapter_id;'
-    for image in Image.objects.raw(sql):
-        listImage.append(image)
-    return listImage
+def getImageUrls():
+    """
+    Lay thong tin tu CSDL de thuc hien viec download image
+    :return: dictionary
+    """
+    #sql= 'SELECT a.id, a.name, b.id, b.name, c.id, c.name, c.url FROM ebook a, chapter b, image c WHERE a.id = b.ebook_id AND b.ebook_id = chapter_id;'
+    filters = Q(status__in=[IMAGE_STATUS_DOWNLOAD_FAILED, IMAGE_STATUS_PENDING])
+    images = Image.objects.filter(filters).values("url", "name", "chapter__name", "chapter_id", "chapter__ebook__id", "chapter__ebook__name")
+    print images
+    return images
+
 if __name__ == '__main__':
-    getEbook()
-    getChapter()
-    getDownload()
+    getEbooks()
+    getChapters()
+    getImageUrls()
