a9b369731ce4d545b241e0deb8084d02b6a3b080
elkun49 <duyth1993@gmail.com>
2015-07-28 21:28:22 +0700

    === Duy:change database_utility === - add logging, log actions inside functions

--- Sources/comic/comicreader/scripts/database_utility.py
+++ Sources/comic/comicreader/scripts/database_utility.py
@@ -2,6 +2,10 @@ __author__ = 'Duy'
 
 import os
 import sys
+import logging
+
+# config logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 parentdir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.insert(0, parentdir)
@@ -22,11 +26,16 @@ def insert_categories(categories):
     :return: 1 if success
              0 otherwise
     """
+    logging.info("Start function insert_categories()")
     try:
+        logging.debug("categories : %s"  %categories)
         for cat in categories:
             cat.save()
-    except:
+    except Exception as inst:
+        logging.error(type(inst))     # the exception instance
+        logging.error(inst)           # __str__ allows args to be printed directly
         return 0
+    logging.info("End function insert_category()")
     return 1
 
 
@@ -37,11 +46,16 @@ def insert_ebooks(ebooks):
     :return: 1 if success
              0 otherwise
     """
+    logging.info("Start function insert_ebooks()")
     try:
+        logging.debug("ebooks : %s"  %ebooks)
         for ebook in ebooks:
             ebook.save()
-    except:
+    except Exception as inst:
+        logging.error(type(inst))
+        logging.error(inst)
         return 0
+    logging.info("End function insert_ebooks()")
     return 1
 
 
@@ -52,11 +66,14 @@ def update_ebook_and_add_bookcat(dict_ebook_cats):
     :return: 1 if success
              0 otherwise
     """
+    logging.info("Start function update_ebook_and_add_bookcat()")
     try:
+        logging.debug("dict_ebook_cats : %s"  %dict_ebook_cats)
         # get and update ebook with provided id
         ebook_update = dict_ebook_cats.get("ebook")
         ebook = Ebook.objects.get(pk=ebook_update.id)
         (ebook_update.url, ebook_update.totalchap) = (ebook.url, ebook.totalchap)
+        logging.debug("ebook_update : %s"  %ebook_update)
         ebook_update.save()
 
         # get categories in db
@@ -65,12 +82,14 @@ def update_ebook_and_add_bookcat(dict_ebook_cats):
         for item in categories_name:
             categories.append(Category.objects.filter(name=item)[0])
 
+        logging.debug("categories : %s"  %categories)
         # update bookcat
         for cat in categories:
             bookcat = Bookcat(ebook=ebook,category=cat)
             bookcat.save()
     except:
         return 0
+    logging.info("End function update_ebook_and_add_bookcat()")
     return 1
 
 
@@ -81,14 +100,19 @@ def insert_chapters(dict_chapters_ebook_id):
     :return: 1 if success
              0 otherwise
     """
+    logging.info("Start function insert_chapters()")
     try:
+        logging.debug("dict_chapters_ebook_id : %s"  %dict_chapters_ebook_id)
         ebook = Ebook.objects.get(pk=dict_chapters_ebook_id.get("ebook_id"))
         for chapter in dict_chapters_ebook_id.get("chapters"):
             chapter.ebook = ebook
             chapter.status = 0
             chapter.save()
-    except:
+    except Exception as inst:
+        logging.error(type(inst))
+        logging.error(inst)
         return 0
+    logging.info("End function insert_chapters()")
     return 1
 
 
@@ -100,28 +124,33 @@ def insert_images(dict_images_chapter_id):
     :return: 1 if success
              0 otherwise
     """
+    logging.info("Start function insert_images()")
     try:
+        logging.debug("dict_images_chapter_id : %s"  %dict_images_chapter_id)
         chapter = Chapter.objects.get(pk=dict_images_chapter_id.get("chapter_id"))
         for image in dict_images_chapter_id.get("images"):
             image.chapter = chapter
             image.status = 0
             image.save()
-    except:
+    except Exception as inst:
+        logging.error(type(inst))
+        logging.error(inst)
         return 0
+    logging.info("End function insert_images()")
     return 1
 
 """ TEST DATA """
 # chapter = Chapter(name="def",url="xyz")
 # chapter2 = Chapter(name="def",url="xyz322323")
-# print insert_chapters({"ebook_id":1, "chapters":[chapter, chapter2]})
+# insert_chapters({"ebook_id":1, "chapters":[chapter, chapter2]})
 
 # image = Image(url="aaa",name="bbb")
-# print insert_images({"chapter_id":3,"images":[image]})
+# insert_images({"chapter_id":3,"images":[image]})
 # cat = Category(name='cat3', description='aloxo')
 # cat2 = Category(name='cat4', description='aloxo')
-# print insert_categories([cat, cat2])
+# insert_categories([cat, cat2])
 #
 # ebook = Ebook(id=1, cover='test',name='yugioh', url='https://blogtruyen.com/yugioh',totalchap=169)
-# print insert_ebooks([ebook])
+# insert_ebooks([ebook])
 
-# print update_ebook_and_add_bookcat({"ebook" : ebook,"categories" : ['cat3','cat4']})
+# update_ebook_and_add_bookcat({"ebook" : ebook,"categories" : ['cat3','cat4']})
