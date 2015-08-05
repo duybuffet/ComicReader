3ffa6ee424214fddd4fb4bb08f4ff3b166c2fee3
phunghieu <phunghieu.ptit@gmail.com>
2015-08-05 11:40:54 +0700

    Hieu: add total image in chapter

--- Sources/comic/comic/settings.py
+++ Sources/comic/comic/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'comic.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'commicreader',
+        'NAME': 'comicreader', #host: comicreader
         'USER': 'root',
-        'PASSWORD' : 'h01629758773',
+        'PASSWORD' : '01629758773', #host: 01629758773
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
     }
