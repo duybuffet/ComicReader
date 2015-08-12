cb8af4bd51a62f1d706c86d692cb65cede50a154
Tăng Hoài Duy <duyth1993@gmail.com>
2015-08-12 23:15:42 +0700

    === Duy down ebook 5067 ===

--- Sources/comic/comicreader/scripts/main_crawl.py
+++ Sources/comic/comicreader/scripts/main_crawl.py
@@ -17,13 +17,15 @@ application = get_wsgi_application()
 from crawl import *
 from database_utility import *
 from database_query_utility import *
+from comicreader.models import *
 
 
 if __name__ == '__main__':
     # insert_categories(crawlCategory())
     #insert_ebooks(crawlAllEbook())
-    for i in range(3, 501):
-        print update_ebook_and_add_bookcat(crawlInforEbook(getEbooksId(i)[0]))
+    # for i in range(3, 501):
+    #     print update_ebook_and_add_bookcat(crawlInforEbook(getEbooksId(i)[0]))
     #for i in range(100, 501):
      #    print insert_images(crawImagesOfChapter(getChapterById(i)[0]))
+     crawlChaptersOfEbook(getEbookById(5067))
     #pass
